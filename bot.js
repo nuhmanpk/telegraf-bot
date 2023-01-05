05c5c2ab4bb072401d61f83289b3f24749726f76
Nuhman Pk <62880706+nuhmanpk@users.noreply.github.com>
2023-01-05 22:45:41 +0530

    Added comments explaining each line

--- bot.js
+++ bot.js
@@ -1,13 +1,14 @@
 const { Telegraf } = require("telegraf");
 
-const bot = new Telegraf("YOUR_BOT_TOKEN_HERE");
-bot.start((ctx) => ctx.reply("I'm Running on Telegraf"));
-bot.command("help", (ctx) => ctx.reply("Help Works"));
-bot.on("sticker", (ctx) =>
+const bot = new Telegraf("YOUR_BOT_TOKEN_HERE"); // better use .env
+
+bot.start((ctx) => ctx.reply("I'm Running on Telegraf")); // will be executed when /start 
+bot.command("help", (ctx) => ctx.reply("Help Works")); // works when you type /help
+bot.on("sticker", (ctx) => // when bot recives a sticker , it reply with a sticker according to the sticker-id mention in replyWithSticker()
   ctx.replyWithSticker(
     "CAACAgUAAxkBAAEFyLNjGfp0a6emv-AbnJUxIrveMiYFggACxQEAAsTTqVSDFrDSj_I_PCkE"
   )
 );
-bot.hears("hello", (ctx) => ctx.reply("Hello from Telegraf"));
+bot.hears("hello", (ctx) => ctx.reply("Hello from Telegraf")); // triggered when bot gets "hello" as a message
 
 bot.launch();
